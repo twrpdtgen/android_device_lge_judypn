831238dce4f596603b80f9c510e923c2ac119148
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-26 13:18:45 +0100

    judypn: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: lge Model: LM-V405 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_judypn.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_judypn-user \
+    omni_judypn-userdebug \
+    omni_judypn-eng
